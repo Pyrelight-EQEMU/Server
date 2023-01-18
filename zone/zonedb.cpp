11e06c21b798216d6aa81f94b3d27d751cb05701
catapultam-habeo <catapultam@pyrelight.net>
2023-01-18 13:49:22 -0600

    ??? I hate C++

--- zone/zonedb.cpp
+++ zone/zonedb.cpp
@@ -1464,7 +1464,10 @@ bool ZoneDatabase::SaveCharacterData(
 	auto results = database.QueryDatabase(query);
 	
 	//Save our current state into multiclass_data table
-	query = StringFormat("REPLACE INTO `multiclass_data` (id,class,level,exp) VALUES (%u,%u,%u,%u)", c->character_id, pp->class_, pp->level, pp->exp);
+
+	uint32 lCharID = c->CharacterID();
+
+	query = StringFormat("REPLACE INTO `multiclass_data` (id,class,level,exp) VALUES (%u,%u,%u,%u)", lCharID, pp->class_, pp->level, pp->exp);
 	results = database.QueryDatabase(query);
 	
 	LogDebug(
