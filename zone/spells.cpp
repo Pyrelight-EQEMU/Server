838a22da5886ba814b34a0c0c4503c52b143a4c8
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-03-03 23:24:47 -0600

    revert that bullshit right there

--- zone/spells.cpp
+++ zone/spells.cpp
@@ -3259,12 +3259,6 @@ bool Mob::CheckSpellLevelRestriction(Mob *caster, uint16 spell_id)
 					can_cast = false;
 				}
 			}
-
-			// Pyrelight Custom Code
-			// let's be WAY more restrictive with buffs
-			if ((GetLevel() - 5) < caster->GetLevel()) {
-				can_cast = false;
-			}
 		}
 	}
 
