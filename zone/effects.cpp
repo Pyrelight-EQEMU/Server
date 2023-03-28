677adb56c530e38b3ef2fda6e829ac10d8a604c1
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-03-28 06:45:43 -0700

    Update zone/effects.cpp
    Co-authored-by: mute <natanx@gmail.com>

--- zone/effects.cpp
+++ zone/effects.cpp
@@ -214,7 +214,7 @@ int64 Mob::GetActSpellDamage(uint16 spell_id, int64 value, Mob* target) {
 		auto damage_reduction_value = 0;
 		if (target->IsClient() && target->GetHeroicSTA() > 0) {
 			damage_redunction_value = RuleI(Character, HeroicWisdomDamageReduction) * target->GetHeroicSTA();
-		} else if (RuleB(Character, ExtraHeroicModifiersForPets) && target->IsPetOwnerClient() && target->GetOwner()->GetHeroicSTR() > 0) {
+		} else if (RuleB(Character, ExtraHeroicModifiersForPets) && target->IsPetOwnerClient() && target->GetOwner()->GetHeroicWIS() > 0) {
 			damage_redunction_value = (2/3) * RuleI(Character, HeroicWisdomDamageReduction) * target->GetHeroicSTA();
 		}
 		value = (std::max(static_cast<int64>(value * RuleR(Character, HeroicWisdomDamageReductionCap) / 100), // Capped Damage Reduction
