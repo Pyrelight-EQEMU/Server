c60ce068fb3295b4966522d929856a852739bcfe
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-06-02 12:46:32 -0500

    test

--- zone/attack.cpp
+++ zone/attack.cpp
@@ -1033,7 +1033,7 @@ int Mob::ACSum(bool skip_caps)
 				shield_ac = CalcRecommendedLevelBonus(GetLevel(), inst->GetItemRecommendedLevel(true), inst->GetItemArmorClass(true));
 			}
 		}
-		shield_ac += IsPetOwnerClient() ? GetOwner()->itembonuses.heroic_str_shield_ac : itembonuses.heroic_str_shield_ac;
+		shield_ac += GetOwner() ? GetOwner()->itembonuses.heroic_str_shield_ac : itembonuses.heroic_str_shield_ac;
 	}
 	// EQ math
 	ac = (ac * 4) / 3;
