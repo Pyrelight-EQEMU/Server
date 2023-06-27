24a02b683b7a5761e2ac05cc51fe78c9f85d7d08
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-06-27 17:23:32 -0500

    wtf

--- zone/attack.cpp
+++ zone/attack.cpp
@@ -2368,10 +2368,11 @@ bool NPC::Attack(Mob* other, int Hand, bool bRiposte, bool IsStrikethrough, bool
 		} 
 		
 		if (my_hit.damage_done > my_hit.original_damage && my_hit.damage_done > 0 && my_hit.original_damage > 0) {
+			LogDebug("Send a hSTR message to [{}] here", this->GetCleanName());
 			int increase_percentage = ((static_cast<float>(my_hit.damage_done) / static_cast<float>(my_hit.original_damage)) - 1) * 100;
-			//if (IsClient()) {			
+			if (IsClient()) {			
 				Message(Chat::YouHitOther, "(Increased by %i%% to %i by your Heroic Strength)", increase_percentage, my_hit.damage_done);
-			//}
+			}
 			if (IsPetOwnerClient() && GetOwner()) {
 				GetOwner()->Message(Chat::OtherHitOther, "(Increased by %i%% to %i by owner's Heroic Strength)", increase_percentage, my_hit.damage_done);
 			}
