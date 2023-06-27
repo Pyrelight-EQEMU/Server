2499ae799c2d5221b2d4b591c16b7a6578e14a5d
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-06-27 17:10:57 -0500

    test

--- zone/attack.cpp
+++ zone/attack.cpp
@@ -2357,10 +2357,12 @@ bool NPC::Attack(Mob* other, int Hand, bool bRiposte, bool IsStrikethrough, bool
 			if (other->IsPetOwnerClient() && other->GetOwner()) {
 				other->GetOwner()->Message(Chat::OtherHitOther, "(Reduced by %i%% from %i by owner's Heroic Stamina)", reduction_percentage, my_hit.original_damage);
 			}
-		} else if (my_hit.damage_done > my_hit.original_damage && my_hit.damage_done > 0 && my_hit.original_damage > 0) {
+		} 
+		
+		if (my_hit.damage_done > my_hit.original_damage && my_hit.damage_done > 0 && my_hit.original_damage > 0) {
 			int increase_percentage = ((static_cast<float>(my_hit.damage_done) / static_cast<float>(my_hit.original_damage)) - 1) * 100;
 			if (IsClient()) {			
-				Message(Chat::OtherHitYou, "(Increased by %i%% to %i by your Heroic Strength)", increase_percentage, my_hit.damage_done);
+				Message(Chat::YouHitOther, "(Increased by %i%% to %i by your Heroic Strength)", increase_percentage, my_hit.damage_done);
 			}
 			if (IsPetOwnerClient() && GetOwner()) {
 				GetOwner()->Message(Chat::OtherHitOther, "(Increased by %i%% to %i by owner's Heroic Strength)", increase_percentage, my_hit.damage_done);
