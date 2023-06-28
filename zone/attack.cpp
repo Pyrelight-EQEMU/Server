ce25c8d77113429b34596820f1d23b5ba240fd50
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-06-28 00:40:06 -0500

    comment

--- zone/attack.cpp
+++ zone/attack.cpp
@@ -4329,6 +4329,7 @@ void Mob::CommonDamage(Mob* attacker, int64 &damage, const uint16 spell_id, cons
 						filter = FilterPetMisses;
 
 					if (!FromDamageShield)
+						// Holy crap this is such a hack to make pet spam visible to owner no matter the distance
 						a->source = owner->GetID();
 						owner->CastToClient()->QueuePacket(outapp, true, CLIENT_CONNECTED, filter);
 						a->Source = attacker->GetID();
