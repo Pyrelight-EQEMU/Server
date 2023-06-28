cea6d2701ab376b52f957562c43f1c9e5a9e6150
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-06-27 23:50:55 -0500

    syntax

--- zone/special_attacks.cpp
+++ zone/special_attacks.cpp
@@ -276,7 +276,7 @@ void Mob::DoSpecialAttackDamage(Mob *who, EQ::skills::SkillType skill, int32 bas
 		if (GetOwner()->IsClient() && GetOwner()->CastToClient()->GetAccountFlag("filter_hSTR") != "off" && IsPetOwnerClient() && GetOwner()) {
 			GetOwner()->Message(Chat::OtherHitOther, "(Increased by %i%% (%i) from %i by owner's Heroic Strength)", increase_percentage, my_hit.damage_done - my_hit.original_damage, my_hit.original_damage);
 		}
-		if IsClient() && CastToClient()->GetAccountFlag("filter_hSTR") != "off") {
+		if (IsClient() && CastToClient()->GetAccountFlag("filter_hSTR") != "off") {
 			Message(Chat::YouHitOther, "(Increased by %i%% (%i) from %i by your Heroic Strength)", increase_percentage, my_hit.damage_done - my_hit.original_damage, my_hit.original_damage);
 		}
 	}
